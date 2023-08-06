5aded1f58057b703b2d6719f9c518aa952f35646
corn <dalcon10280@gmail.com>
2023-08-06 11:21:38 +0900

    theme

--- src/App.svelte
+++ src/App.svelte
@@ -25,58 +25,41 @@ const onSignInInHandler = async () => {
 }
 </script>
 
-<main class="flex flex-col">
-  <section class="flex flex-col gap-5">
-    <h1 class="text-3xl font-bold text-center">
-      Web View Test Viewer
-    </h1>
-    <div class="card bg-neutral text-neutral-content">
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">이벤트 목록</h2>
-        <div class="card-actions justify-end">
-          <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-primary" on:click={onSignInInHandler}>signInInHandler</button> 
-        </div>
+<main class="flex flex-col p-4 md:p-8">
+  <h1 class="mb-6 text-3xl font-bold text-center md:mb-10">
+    Web View Test Viewer
+  </h1>
+
+  <section class="mb-4 card bg-neutral text-neutral-content md:mb-8">
+    <div class="items-center text-center card-body">
+      <h2 class="card-title">이벤트 목록</h2>
+      <div class="justify-end card-actions">
+        <button class="normal-case btn btn-sm btn-primary" on:click={onSignInInHandler}>signInInHandler</button> 
       </div>
     </div>
-    <h1 class="text-2xl font-bold text-center">
-      받은 콜백
-    </h1>
-    <div class="mockup-code">
-      <pre data-prefix=">" class="text-warning"><code>{JSON.stringify(callbackMessage)}</code></pre> 
-    </div>
   </section>
-  
-  <!-- <div class="card">
+
+  <h1 class="mb-4 text-2xl font-bold text-center md:mb-8">
+    받은 콜백
+  </h1>
+  <div class="mb-6 mockup-code md:mb-10">
+    <pre data-prefix=">" class="text-warning"><code>{JSON.stringify(callbackMessage)}</code></pre> 
+  </div>
+
+  <!-- 아래 주석처리된 부분은 필요에 따라 추가적인 컴포넌트를 렌더링하는 예시입니다. -->
+  <!-- <div class="mb-4 card md:mb-8">
     <h3>isFlutterInAppWebViewReady: {isFlutterInAppWebViewReady}</h3>
     <h3>customEventDetail: {JSON.stringify(customEventDetail, null, 2)}</h3>
   </div>
-  <div class="card">
+  <div class="mb-4 card md:mb-8">
     <Counter />
   </div>
-  <div class="card">
+  <div class="mb-4 card md:mb-8">
     <h2>post message</h2>
-    <textarea id="postMessage" rows="5" cols="50"></textarea>
+    <textarea id="postMessage" rows="5" class="w-full p-2 border rounded-lg" placeholder="Enter your message"></textarea>
     <br />
-    <button on:click={onPostMessage}>
+    <button class="mt-2 btn btn-primary" on:click={onPostMessage}>
       post
     </button>
   </div> -->
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
